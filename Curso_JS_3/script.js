462ea67736ab57df0e8876d5bba197771d5039f6
NightMare <terremoto.net5@gmail.com>
2024-07-09 20:10:45 -0600

    curso_js3_3

---
+++ Curso_JS_3/script.js
@@ -0,0 +1,13 @@
+const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5";
+
+fetch(url)
+
+const solicitud = new Promise((resolve, reject) => {
+    const response = "resolve"
+
+    if (response = "resolve") {
+        resolve("La promesa se ha completado con éxito")
+    } else {
+        reject("La promesa ha fallado")
+    }
+})

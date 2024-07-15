7b853fe80d51a0e8ef99aa11c51c287154f2077b
NightMare <terremoto.net5@gmail.com>
2024-07-15 01:17:31 -0600

    curso_js5_4

--- Curso_JS_5/js/validar-cuil.js
+++ Curso_JS_5/js/validar-cuil.js
@@ -3,11 +3,13 @@ export default function esUnCuil(campo){
     
     if(tieneNumerosRepetidos(cuil)){
         console.log("El CUIL tiene números repetidos");
+        campo.setCustomValidity("Valores repetidos");
     }else{
         if(validarPrimerosDigitos(cuil) && validarDigitoVerificador(cuil)){
             console.log("El CUIL es válido");
         }else{
             console.log("El CUIL no es válido");
+            campo.setCustomValidity("CUIL inválido");
         }
     }
 }
